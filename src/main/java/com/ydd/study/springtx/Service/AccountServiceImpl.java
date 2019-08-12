9aa2f986e04070951985317585d668c1afa35443
yindd <695685009@qq.com>
2019-08-12 20:54:50 +0800

    spring 代码配置事务

--- src/main/java/com/ydd/study/springtx/Service/AccountServiceImpl.java
+++ src/main/java/com/ydd/study/springtx/Service/AccountServiceImpl.java
@@ -1,14 +1,28 @@
 package com.ydd.study.springtx.Service;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
 import com.ydd.study.springtx.Dao.AccountDao;
 public class AccountServiceImpl  implements AccountService {
 	private AccountDao accountDao;
+	private TransactionTemplate tt;
 
+	
 	@Override
-	public void transer(int from, int to, double money) {
-		accountDao.decreseMoney(from, money);
-		accountDao.increseMoney(to, money);
+	public void transer(final int from, final int to, final double money) {
+		tt.execute(new TransactionCallbackWithoutResult() {
+			
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				accountDao.decreseMoney(from, money);
+				int i = 1/0;
+				accountDao.increseMoney(to, money);
 
+			}
+		});
+		
 	}
 
 	public AccountDao getAccountDao() {
@@ -18,4 +32,14 @@ public class AccountServiceImpl  implements AccountService {
 	public void setAccountDao(AccountDao accountDao) {
 		this.accountDao = accountDao;
 	}
+
+	public TransactionTemplate getTt() {
+		return tt;
+	}
+
+	public void setTt(TransactionTemplate tt) {
+		this.tt = tt;
+	}
+	
+	
 }
